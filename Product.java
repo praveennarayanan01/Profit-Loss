4060ec8fbfaf902087f4c3004d984d75a3deef44
Praveen <109944476+praveennarayanan01@users.noreply.github.com>
2024-02-06 10:54:54 +0530

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,13 @@
+class Product{
+ String productName;
+ double productValue;
+  
+  public Product(String pName,double pValue){
+    productName=pName;
+    productValue=pValue;
+ }
+ 
+  public static void main(String[]args){
+    
+ }
+}
