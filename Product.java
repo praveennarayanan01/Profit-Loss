5e72ba54825833be64cebb05f8b78448c862e176
Praveen <109944476+praveennarayanan01@users.noreply.github.com>
2024-02-06 11:09:58 +0530

    Update Product.java

--- Product.java
+++ Product.java
@@ -1,11 +1,5 @@
 class Product{
- String productName;
- double productValue;
-  
-  public Product(String pName,double pValue){
-    productName=pName;
-    productValue=pValue;
- }
+ 
  
   public static void main(String[]args){
     
